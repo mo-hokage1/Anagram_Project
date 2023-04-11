56a10c6250b00e452796ecc79c61bcf65c7ed3b2
Mohamed Said Abdulqadir <80471511+mohamed-abdulqadir@users.noreply.github.com>
2023-04-11 15:28:19 +0100

    Add files via upload
    Optimized space usage

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
 // Require the fs module
 const fs = require('fs');
+const readline = require('readline');
 
 // Get the command line arguments and extract the filename from the arguments
 const args = process.argv.slice(2);
 const filename = args[0];
 
-// Read the file and if there's an error, log the error message
-fs.readFile(filename, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
+// Create a readable stream to read the file line by line
+const readStream = fs.createReadStream(filename, 'utf-8');
+const rl = readline.createInterface({
+  input: readStream,
+  output: process.stdout,
+  terminal: false
+});
+
+const groups = {}; // Create an empty object to hold the anagram groups
+
+// Listen for 'line' event to process each line in the file
+rl.on('line', (line) => {
+  // Sort the letters of the word and join them back together e.g (car -> acr)
+  const sorted = line.split('').sort().join('').replace('\r', '');
+
+  // If the sorted letters haven't been seen before, create a new empty array for them
+  if (!groups[sorted]) {
+    groups[sorted] = [];
   }
+  // Add the word to the array of anagrams for the current sorted letters
+  groups[sorted].push(line);
+});
 
-  // Split the file contents into an array of words
-  const words = data.trim().split('\n');
-  const groups = {};  // Create an empty object to hold the anagram groups
-
-  for (let word of words) {
-    // Sort the letters of the word and join them back together e.g (car -> acr)
-    const sorted = word.split('').sort().join('').replace('\r', '');
-
-    // If the sorted letters haven't been seen before, create a new empty array for them
-    if (!groups[sorted]) {
-      groups[sorted] = [];
-    }
-    // Add the word to the array of anagrams for the current sorted letters
-    groups[sorted].push(word);
-  }
-
-  const result = [];
+// Listen for 'close' event to handle end of file
+rl.on('close', () => {
+  const writeStream = fs.createWriteStream('output.txt'); // Create a writable stream to write the output to a file
+  const output = []; // Create an array to hold the output strings
 
   // Loop through each set of sorted letters in the object
   for (let sorted of Object.keys(groups)) {
     const group = groups[sorted]; // Get the array of anagrams for the current set of sorted letters
-    result.push(group.join(' ')); // Join the anagrams into a single string, separated by spaces
+    writeStream.write(group.join(' ') + '\n'); // Write the joined anagrams to the writable stream
+    output.push(group.join(' ')); // Push the joined anagrams to the output array
   }
 
-   // Create a new array that removes any carriage return characters and commas from the result
-  const output = result.map(str => str.replace(/\r/g, '').replace(/,/g, '') + '\n');;
-  console.log(output.join('')); // Print the output to the console as strings
+  writeStream.end(); // Close the writable stream after writing is complete
+  console.log('Anagram groups written to output.txt'); // Print a message to indicate completion
+  console.log(output.join('\n')); // Print the output to the console
 });
